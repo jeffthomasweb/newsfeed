b302a48cfdeaf6ec541379c2a4321eadc239909b
jeffthomasweb <93561509+jeffthomasweb@users.noreply.github.com>
2021-11-26 13:51:42 -0500

    Removed f-strings

--- app.py
+++ app.py
@@ -13,63 +13,63 @@ app = Flask(__name__)
 
 a = feedparser.parse("https://feeds.npr.org/1001/rss.xml")
 
-b = f"{a.entries[0].title + '. ' + a.entries[0].summary}"
+b = a.entries[0].title + '. ' + a.entries[0].summary
 b1 = re.sub('<em>', '',b)
 b2 = re.sub('</em>', '',b1)
 
-c = f"{a.entries[1].title + '. ' + a.entries[1].summary}"
+c = a.entries[1].title + '. ' + a.entries[1].summary
 c1 = re.sub('<em>', '',c)
 c2 = re.sub('</em>', '',c1)
 
-d = f"{a.entries[2].title + '. ' + a.entries[2].summary}"
+d = a.entries[2].title + '. ' + a.entries[2].summary
 d1 = re.sub('<em>', '',d)
 d2 = re.sub('</em>', '',d1)
 
-e = f"{a.entries[3].title + '. ' + a.entries[3].summary}"
+e = a.entries[3].title + '. ' + a.entries[3].summary
 e1 = re.sub('<em>', '',e)
 e2 = re.sub('</em>', '',e1)
 
-f = f"{a.entries[4].title + '. ' + a.entries[4].summary}"
+f = a.entries[4].title + '. ' + a.entries[4].summary
 f1 = re.sub('<em>', '',f)
 f2 = re.sub('</em>', '',f1)
 
-g = f"{a.entries[5].title + '. ' + a.entries[5].summary}"
+g = a.entries[5].title + '. ' + a.entries[5].summary
 g1 = re.sub('<em>', '',g)
 g2 = re.sub('</em>', '',g1)
 
-h = f"{a.entries[6].title + '. ' + a.entries[6].summary}"
+h = a.entries[6].title + '. ' + a.entries[6].summary
 h1 = re.sub('<em>', '',h)
 h2 = re.sub('</em>', '',h1)
 
-i = f"{a.entries[7].title + '. ' + a.entries[7].summary}"
+i = a.entries[7].title + '. ' + a.entries[7].summary
 i1 = re.sub('<em>', '',i)
 i2 = re.sub('</em>', '',i1)
 
-j = f"{a.entries[8].title + '. ' + a.entries[8].summary}"
+j = a.entries[8].title + '. ' + a.entries[8].summary
 j1 = re.sub('<em>', '',j)
 j2 = re.sub('</em>', '',j1)
 
-k = f"{a.entries[9].title + '. ' + a.entries[9].summary}"
+k = a.entries[9].title + '. ' + a.entries[9].summary
 k1 = re.sub('<em>', '',k)
 k2 = re.sub('</em>', '',k1)
 
-l = f"{a.entries[10].title + '. ' + a.entries[10].summary}"
+l = a.entries[10].title + '. ' + a.entries[10].summary
 l1 = re.sub('<em>', '',l)
 l2 = re.sub('</em>', '',l1)
 
-m = f"{a.entries[11].title + '. ' + a.entries[11].summary}"
+m = a.entries[11].title + '. ' + a.entries[11].summary
 m1 = re.sub('<em>', '',m)
 m2 = re.sub('</em>', '',m1)
 
-n = f"{a.entries[12].title + '. ' + a.entries[12].summary}"
+n = a.entries[12].title + '. ' + a.entries[12].summary
 n1 = re.sub('<em>', '',n)
 n2 = re.sub('</em>', '',n1)
 
-o = f"{a.entries[13].title + '. ' + a.entries[13].summary}"
+o = a.entries[13].title + '. ' + a.entries[13].summary
 o1 = re.sub('<em>', '',o)
 o2 = re.sub('</em>', '',o1)
 
-p = f"{a.entries[14].title + '. ' + a.entries[14].summary}"
+p = a.entries[14].title + '. ' + a.entries[14].summary
 p1 = re.sub('<em>', '',p)
 p2 = re.sub('</em>', '',p1)
 
